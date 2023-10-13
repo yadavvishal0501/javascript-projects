07ed7ac7e1f30fa94e63fa93d5f7bbaa1838df8c
vishal <vy20011945@gmail.com>
2023-10-13 14:03:33 +0530

    solved keypad issue

--- calculator/app.js
+++ calculator/app.js
@@ -3,7 +3,7 @@ const num = document.querySelectorAll('.num')
 const buttons = document.querySelectorAll('input[type=button]')
 
 
-let output= ''
+let output = ''
 
 // using for each as the buttons is an array of buttons 
 
@@ -11,9 +11,9 @@ let output= ''
 buttons.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         // btn.classList because it is an for individual button classes
-        if (btn.classList[0] == 'num') {
+        if (btn.className == 'num') {
             output += e.target.value;
-        } else if (btn.classList[0] == 'operator') {
+        } else if (display.value !== '' && btn.classList[0] == 'operator') {
             output += e.target.value;
         } else if (btn.classList[0] == 'eval') {
             // eval() to evaluate total
@@ -21,10 +21,70 @@ buttons.forEach((btn) => {
         } else if (btn.classList[0] == 'clear') {
             output = '';
         }
-        
+
         display.value = output;
-    
-});
+
+    });
 })
+document.onkeydown = function (e) {
+    // console.log(e);
+    switch (e.key) {
+        case '+':
+            if (display.value !== '') {
+                output += '+';
+                display.value = output;
+            }
+            break;
+        case '-':
+            if (display.value !== '') {
+                output += '-';
+                display.value = output;
+            }
+
+            break;
+        case '*':
+            if (display.value !== '') {
+                output += '*';
+                display.value = output;
+            }
+            break;
+        case '/':
+            if (display.value !== '') {
+                output += '/';
+                display.value = output;
+            }
+            break;
+        case '.':
+            output += '.';
+            display.value = output;
+
+            break;
+
+
+        case 'Enter':
+            output = eval(output);
+            display.value = output;
+
+            break;
+
+        case 'Backspace':
+            output = parseInt(String(output).split('').slice(0, -1).join(''));
+            display.value = output;
+            break;
+
+        case 'A':
+            output = '';
+            display.value = output;
+
+            break;
+
+        default:
+            if (e.which > 47 && e.which < 58) {
+                output += e.key;
+                display.value = output;
+            }
+    }
+
+}
 
 //
